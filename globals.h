b904343bce37cbf997fadd0868d2e60a4e2ce32f
Egor00f <148970641+Egor00f@users.noreply.github.com>
2023-11-08 10:57:21 +0500

    Update globals.h

--- globals.h
+++ globals.h
@@ -5,47 +5,37 @@
 #include "button.h"
 
 
-struct CONFIG {
-	//Пороговые значения	
-	int16_t MIN_GAS;
-	int16_t MAX_GAS;
+namespace global
+{
 
-	bool LEDS;		//Светодиоды
-	char lang;		//Язык
-};
+	//звук
+	soundOn    = false;//по умолчанию звук
+	bool soundSpeed = false;
 
-//=================Глобальные переменные ======================================
+	uint16_t DELAY  = SOUNDSPEEDLOW; //переменная 	для сохранения скорости пищания
 
-//звук
-bool soundOn    = false;		//по умолчанию звук не воспроизводим
-bool soundSpeed = false;
+	unsigned long time;
 
-uint16_t DELAY  = SOUNDSPEEDLOW; //переменная для сохранения скорости пищания
-
-unsigned long time;
-
-bool mute = false;
+	bool mute = false;
 
 
-uint8_t pinR = LED_PIN_RED;
-uint8_t pinG = LED_PIN_GREEN;
-uint8_t pinB = LED_PIN_BLUE;
+	uint8_t pinR = LED_PIN_RED;
+	uint8_t pinG = LED_PIN_GREEN;
+	uint8_t pinB = LED_PIN_BLUE;
 
-#ifdef LED_PIN_YELLOW
-uint8_t pinY = LED_PIN_YELLOW;
-#endif
-
-
-//Управляющие кнопки
-button leftButton(BUTTON_LEFT_PIN);
-button rightButton(BUTTON_RIGHT_PIN);
-button centerButton(BUTTON_CENTER_PIN);
+	#ifdef LED_PIN_YELLOW
+	uint8_t pinY = LED_PIN_YELLOW;
+	#endif
 
 
-CONFIG config;	//Экземпляр настроек
+	//Управляющие кнопки
+	button leftButton(BUTTON_LEFT_PIN);
+	button rightButton(BUTTON_RIGHT_PIN);
+	button centerButton(BUTTON_CENTER_PIN);
 
-//==============================================================================
 
+	CONFIG config;	//Экземпляр настроек
 
+}
 
 #endif
